281c6a8dfa6ce3a9af3b65f758324fcc72d48ef5
Vinayak Vilas Mote <Vinayak@ssss-MacBook-Pro.local>
2019-07-26 12:59:13 +0530

    set up create-react-app with support for .scss files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      CAR LISTING APP COMING SOON
+    </div>
+  );
+}
+
+export default App;
